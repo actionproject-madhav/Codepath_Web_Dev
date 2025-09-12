368f1cd0245e018bc00fa7cff488d8b4aea39691
madhav <madhav@madhavs-air.rollins.edu>
2025-09-12 16:31:07 -0400

    added initial logic

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Serve static files from "public" folder (frontend)
-app.use(express.static(path.join(__dirname, "public")));
+// Middleware to serve static files
+app.use(express.static('public'));
 
-// Example API endpoint
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from Express backend!" });
+// Set view engine (we'll use basic HTML files)
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Coffee shop detail routes (we'll add these next)
+app.get('/coffee-shops/:id', (req, res) => {
+    const shopId = req.params.id;
+    // We'll implement this logic next
+    res.sendFile(path.join(__dirname, 'public', 'detail.html'));
+});
+
+// 404 handler - must be last
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
